7691c6a186e914bcb4067373302685d9905cc4ce
edias15 <emerson15dias@hotmail.com>
2021-11-15 08:43:31 -0300

    Some adjusts to run.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ oauth2Client.setCredentials({ refresh_token: refresh })
 
 const drive = google.drive({ version: 'v3', auth: oauth2Client })
 const filePath = join(__dirname, 'test.json')
+let fileId
 
 async function uploadFile (filePath) {
   try {
@@ -27,10 +28,43 @@ async function uploadFile (filePath) {
       }
     })
     console.log(response.data)
+    return response.data.id
   } catch (error) {
     console.log(error)
   }
 }
 
-
 uploadFile(filePath)
+
+async function deleteFile (fileId) {
+  try {
+    const response = await drive.files.delete({
+      fileId: fileId
+    })
+    console.log(response.data, response.status)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+async function generatePublicUrl () {
+  try {
+    const response = await drive.permissions.create({
+      fileId: fileId,
+      requestBody: {
+        role: 'reader',
+        type: 'anyone'
+      }
+    })
+    console.log(response.data)
+    const result = await drive.files.get({
+      fileId: fileId,
+      fields: 'webViewLink, webContentLink'
+    }).then(
+      console.log(result)
+      )
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
